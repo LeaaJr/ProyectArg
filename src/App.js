0a90e3822df260387993feb099df6f96705a030e
LeaaJr <leeitooocabj@gmail.com>
2024-10-16 23:53:23 +0200

    Fix-Footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
-import Productos from './Components/Productos';
 import Navbar from './Components/Navbar';
-// Asegúrate de que la ruta de importación sea correcta
+import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos/:categoria" element={<Productos />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Otras rutas */}
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
 }
 
-export default App;
+export default App;  // Asegúrate de que esta línea esté presente
